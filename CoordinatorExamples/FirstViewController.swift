16a77013ec1224ee8fc1b5f4ee4b92bcfa4559a1
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-10-14 17:37:06 +0200

    [+] Initial commit: xcodeproj and target created

---
+++ CoordinatorExamples/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CoordinatorExamples
+//
+//  Created by Mathis Detourbet on 14/10/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoordinatorExamples/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CoordinatorExamples
+//
+//  Created by Mathis Detourbet on 14/10/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
