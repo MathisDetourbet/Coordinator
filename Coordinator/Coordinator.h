537dea9fa576c8629feb0d1f92013f5abe22f678
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-10-14 17:12:41 +0200

    Initial Commit

---
+++ Coordinator/Coordinator.h
@@ -0,0 +1,19 @@
+//
+//  Coordinator.h
+//  Coordinator
+//
+//  Created by Mathis Detourbet on 14/10/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Coordinator.
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+
+//! Project version string for Coordinator.
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordinator/PublicHeader.h>
+
+
