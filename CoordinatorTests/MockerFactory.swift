15b0895e002cb1b9371d4460cecfbe8c963f435d
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-10-15 11:11:57 +0200

    add NavCoordinator unit tests

---
+++ CoordinatorTests/MockerFactory.swift
@@ -0,0 +1,9 @@
+//
+//  MockerFactory.swift
+//  CoordinatorTests
+//
+//  Created by Mathis Detourbet on 14/10/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import Foundation
