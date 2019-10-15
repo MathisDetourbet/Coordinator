317eb2847d0ff78a8d418371c432896c38f57c31
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-10-15 14:00:42 +0200

    [+] add TabBarCoordinator unit tests

---
+++ CoordinatorTests/TabBarCoordinatorTests.swift
@@ -0,0 +1,24 @@
+//
+//  TabCoordinatorTests.swift
+//  CoordinatorTests
+//
+//  Created by Mathis Detourbet on 15/10/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import XCTest
+@testable import Coordinator
+
+class TabCoordinatorTests: XCTestCase {
+    
+    var tabCoordinator: TabCoordinator!
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
